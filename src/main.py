08c61b6bb22e5bb3f1d1c900c5b1035503dae7f5
phxgg <stamatis.1702@gmail.com>
2023-01-20 06:22:40 +0200

    testing

--- src/main.py
+++ src/main.py
@@ -3,7 +3,24 @@ from flask import Flask, request, send_file
 from yt_dlp import YoutubeDL
 from contextlib import redirect_stdout
 import io
-from utils import slugify
+import unicodedata
+import re
+
+def slugify(value, allow_unicode=False):
+  """
+  Taken from https://github.com/django/django/blob/master/django/utils/text.py
+  Convert to ASCII if 'allow_unicode' is False. Convert spaces or repeated
+  dashes to single dashes. Remove characters that aren't alphanumerics,
+  underscores, or hyphens. Convert to lowercase. Also strip leading and
+  trailing whitespace, dashes, and underscores.
+  """
+  value = str(value)
+  if allow_unicode:
+    value = unicodedata.normalize('NFKC', value)
+  else:
+    value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore').decode('ascii')
+  value = re.sub(r'[^\w\s-]', '', value.lower())
+  return re.sub(r'[-\s]+', '-', value).strip('-_')
 
 app = Flask(__name__)
 
